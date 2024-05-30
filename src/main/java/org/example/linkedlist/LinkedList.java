c1849000b8fd96bcb9e67d77bed08725d44e080d
Lev Nagornov <levnagornov@yahoo.com>
2024-05-30 13:44:23 +0200

    feat: create initial LinkedList class

---
+++ src/main/java/org/example/linkedlist/LinkedList.java
@@ -0,0 +1,65 @@
+package org.example.linkedlist;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    public LinkedList(int value) {
+        head = new Node(value);
+        tail = head;
+        length = 1;
+    }
+
+    public class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length: " + length);
+        System.out.println("\nLinkedList:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+}
